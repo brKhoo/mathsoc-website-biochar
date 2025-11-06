1e49aa39b31ce6d334c22bd1379e9c76b72bb955
lorenaguo <lorenaguo22@gmail.com>
2025-11-05 23:02:51 -0500

    F25 updates (#16)

--- src/app/community/leadership/page.tsx
+++ src/app/community/leadership/page.tsx
@@ -25,52 +25,99 @@ const Council: React.FC = () => {
       <h2>Councillors</h2>
       <HeadshotGrid>
         <Headshot
-          name="Sara Nayar"
-          position="PMAMC&O Representative"
-          email="s5nayar@uwaterloo.ca"
-          image="/img/councillor-images/snayar.jpg"
-        />
-        <Headshot
-          name="Amol Venkataraman"
+          name="River Stanley"
           position="Computer Science Representative"
-          email="avenkataraman@uwaterloo.ca"
+          email="river.stanley@uwaterloo.ca"
         />
         <Headshot
-          name="Cristian Moretto"
-          position="Computer Science Representative"
-          email="cmoretto@uwaterloo.ca"
-        />
-        <Headshot
-          name="Grace Feng"
-          position="Computer Science Representative"
-          email="g27feng@uwaterloo.ca"
-          image="/img/councillor-images/gfeng.jpg"
-        />
-        <Headshot
-          name="Faraan Hadi"
-          position="Computer Science Representative"
-          email="faraan.hadi@uwaterloo.ca"
-        />
-        <Headshot
-          name="Saivenkat Jilla"
+          name="Sai Jilla"
           position="Computer Science Representative"
           email="sljilla@uwaterloo.ca"
-          image="/img/councillor-images/sjilla.jpg"
         />
         <Headshot
-          name="Alex Lavallee"
-          position="At-Large Representative"
-          email="a2lavallee@uwaterloo.ca"
+          name="Avish Kathpal"
+          position="Computer Science Representative"
+          email="akathpal@uwaterloo.ca"
         />
         <Headshot
-          name="Alina Hu"
-          position="At-Large Representative"
-          email="alina.hu@uwaterloo.ca"
+          name="Elyn Huang"
+          position="Computer Science Representative"
+          email="e73huang@uwaterloo.ca"
         />
         <Headshot
-          name="River Stanley"
+          name="Xiangru Mo"
+          position="Business Representative"
+          email="x6mo@uwaterloo.ca"
+        />
+        <Headshot
+          name="Aleksa Misic"
+          position="Software Engineering Representative"
+          email="a2misic@uwaterloo.ca"
+        />
+        <Headshot
+          name="Maggie Chen"
+          position="Software Engineering Representative"
+          email="m29chen@uwaterloo.ca"
+        />
+        <Headshot
+          name="E-Therng Lee"
           position="At-Large Representative"
-          email="river.stanley@uwaterloo.ca"
+          email="e-therng.lee@uwaterloo.ca"
+        />
+        <Headshot
+          name="Shalev Manor"
+          position="At-Large Representative"
+          email="smanor@uwaterloo.ca"
+        />
+        <Headshot
+          name="Shayan Jizan"
+          position="At-Large Representative"
+          email="sjizan@uwaterloo.ca"
+        />
+        <Headshot
+          name="Nicholas Rebello"
+          position="At-Large Representative"
+          email="nrebello@uwaterloo.ca"
+        />
+        <Headshot
+          name="Samantha Pater"
+          position="At-Large Representative"
+          email="snpater@uwaterloo.ca"
+        />
+        <Headshot
+          name="Wendy Yang"
+          position="At-Large Representative"
+          email="wz2yang@uwaterloo.ca"
+        />
+        <Headshot
+          name="Samir Sharma"
+          position="At-Large Representative"
+          email="samir.sharma@uwaterloo.ca"
+        />
+        <Headshot
+          name="Aeshaan Kumarm"
+          position="First Year Representative"
+          email="a386kumar@uwaterloo.ca"
+        />
+        <Headshot
+          name="Angela Li"
+          position="First Year Representative"
+          email="ay28li@uwaterloo.ca"
+        />
+        <Headshot
+          name="Catherine Chen"
+          position="First Year Representative"
+          email="c623chen@uwaterloo.ca"
+        />
+        <Headshot
+          name="Ryan Qiao"
+          position="First Year Representative"
+          email="y22qiao@uwaterloo.ca"
+        />
+        <Headshot
+          name="Suri Tian"
+          position="First Year Representative"
+          email="s48tian@uwaterloo.ca"
         />
       </HeadshotGrid>
     </div>
@@ -85,22 +132,56 @@ const Board: React.FC = () => {
       <h2>Directors</h2>
       <HeadshotGrid>
         <Headshot
-          name="Awab Qureshi"
+          name="Jack Whittick"
           position="Chair, At-Large Director"
           email="chair@mathsoc.uwaterloo.ca"
+          image="/img/councillor-images/jwhittick.jpg"
+        />
+        <Headshot
+          name="Awab Qureshi"
+          position="At-Large Director"
+          email="a9quresh@uwaterloo.ca"
           image="/img/councillor-images/aqureshi.webp"
         />
         <Headshot
-          name="Grace Feng"
+          name="Saivenkat Jilla"
           position="At-Large Director"
-          email="g27feng@uwaterloo.ca"
-          image="/img/councillor-images/gfeng.jpg"
+          email="sljilla@uwaterloo.ca"
         />
         <Headshot
-          name="Jack Whittick"
+          name="Nihal Mir"
           position="At-Large Director"
-          email="jbwhitti@uwaterloo.ca"
-          image="/img/councillor-images/jwhittick.jpg"
+          email="n2mir@uwaterloo.ca"
+        />
+        <Headshot
+          name="Isabela Souza"
+          position="At-Large Director"
+          email="isabelacefrin@gmail.com"
+        />
+        <Headshot
+          name="Amir Dadpour"
+          position="At-Large Director"
+          email="amirhossein.dadpour@gmail.com"
+        />
+        <Headshot
+          name="Avish Kathpal"
+          position="At-Large Director"
+          email="akathpal@uwaterloo.ca"
+        />
+        <Headshot
+          name="Patrik Buhring"
+          position="At-Large Director"
+          email="patrikbuhring@gmail.com"
+        />
+        <Headshot
+          name="Elyn Huang"
+          position="Councillor-Director"
+          email="elyn.huang@uwaterloo.ca"
+        />
+        <Headshot
+          name="E-Therng Lee"
+          position="Councillor-Director"
+          email="e-therng.lee@uwaterloo.ca"
         />
         <Headshot
           name="Danya Matlin"
